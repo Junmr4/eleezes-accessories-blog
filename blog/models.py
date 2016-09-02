e751450a131a625c8269e7b2ef63d9d042d29c50
Alfredo Moreno <junmr4@gmail.com>
2016-09-02 16:45:13 -0400

    Created the post and comment models and configure postgres to project

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.db.models import permalink
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):                                                                      
+    title = models.CharField(max_length=256)
+    body = models.TextField()                                                 
+    created_at = models.DateTimeField('Datetime created') 
+    def __unicode__(self):
+    	return self.title                                   
+                                                                                          
+                                                                                          
+class Comment(models.Model):                                                                   
+    post = models.ForeignKey(Post)                                                             
+    message = models.TextField()                                                               
+    created_at = models.DateTimeField('Datetime created')
+    def __unicode__(self):
+    	return self.message
--- eleezeaccessories/settings.py
+++ eleezeaccessories/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'eleezeaccessories.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogdb',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
